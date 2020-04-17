5c0b099096a1f37df8a1c6511f1b471bd0a12e3e
dickygiancini <dicky.giancini.123@gmail.com>
2020-04-17 11:48:55 +0700

    First Commit Dicky

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('template/main')
+
+@section('title', 'Pemesanan Bioskop')
+
+@section('container')
+    
+    <div class="container">
+      <h1>Hello, world!</h1>
+    </div>
+@endsection
